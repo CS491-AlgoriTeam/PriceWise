4726858a53c61cf6cfca6ac838d5d34856b50a12
MeteArikan <mete.arikan01@gmail.com>
2023-12-15 02:05:32 +0300

    Code ProductSeller page
    All data in ProductSeller page static. Also  changed the navigation route of ProductSeller page in SearchProductsPage.

--- front-end/lib/pages/ProductSellers.dart
+++ front-end/lib/pages/ProductSellers.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pwfe/classes/Product.dart';
+import 'package:pwfe/components/bars/navigation_bar_bottom.dart';
 
 class ProductSellers extends StatefulWidget {
+  final Product product;
+
   const ProductSellers({
     Key? key,
+    required this.product,
   }) : super(key: key);
 
   @override
@@ -11,13 +15,45 @@ class ProductSellers extends StatefulWidget {
 }
 
 class _ProductSellersState extends State<ProductSellers> {
+  // Sample data, replace this with your actual data
+  final List<Map<String, dynamic>> sellersData = [
+    {'sellerName': 'Seller A', 'price': 20.0},
+    {'sellerName': 'Seller B', 'price': 25.0},
+    {'sellerName': 'Seller C', 'price': 18.0},
+    // Add more sellers as needed
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sellers Profile'),
+        title: Text('Seller Profile'),
       ),
-      body: CustomScrollView(),
+      bottomNavigationBar: navigation_bar_bottom(context),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Sellers of ${widget.product.getProductName()}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: sellersData.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(sellersData[index]['sellerName']),
+                  subtitle: Text('Price: \$${sellersData[index]['price']}'),
+                  // You can customize the ListTile as needed
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
