d3dc54ea7a3702d5539c950dbe547fbdb223236d
Mehmet Ali Öztürk <ali.ozturk@ug.bilkent.edu.tr>
2024-05-09 18:14:19 +0300

    product suggestion
    ui needs to be changed

--- front-end/lib/pages/ShoppingListDetailsPage.dart
+++ front-end/lib/pages/ShoppingListDetailsPage.dart
@@ -160,8 +160,7 @@ import 'package:pwfe/components/buttons/button_blue_lighter_rounded.dart';
 import 'package:pwfe/pages/ShowSuggestedListsPage.dart';
 
 class ShoppingListDetailsPage extends StatefulWidget {
-  final String
-      listId; // Assuming you pass the specific list ID when navigating to this page.
+  final String listId;
   ShoppingListDetailsPage({Key? key, required this.listId}) : super(key: key);
 
   @override
@@ -172,15 +171,26 @@ class ShoppingListDetailsPage extends StatefulWidget {
 class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
   List<Map<String, dynamic>> items = [];
   double total = 0;
+<<<<<<< Updated upstream
+=======
+  final User? user = FirebaseAuth.instance.currentUser;
+  List<DocumentSnapshot> _shoppingLists = [];
+  List<Map<String, dynamic>> suggestedItems = [];
+  bool isLoading = true;
+>>>>>>> Stashed changes
 
   @override
   void initState() {
     super.initState();
     _fetchShoppingListItems();
+<<<<<<< Updated upstream
+=======
+    _fetchShoppingLists();
+    _fetchSuggestedItems();
+>>>>>>> Stashed changes
   }
 
   void _fetchShoppingListItems() async {
-    // Assuming your items are stored in a subcollection or referenced by ID within each shopping list document
     FirebaseFirestore.instance
         .collection('shoppingLists')
         .doc(widget.listId)
@@ -191,7 +201,7 @@ class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
       List<Map<String, dynamic>> tempList = [];
       for (var doc in snapshot.docs) {
         Map<String, dynamic> item = doc.data();
-        item['id'] = doc.id; // Keep the document ID for later operations
+        item['id'] = doc.id;
         tempList.add(item);
         tempTotal += item['price'] * item['amount'];
       }
@@ -204,7 +214,6 @@ class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
   }
 
   void _updateItemAmount(String itemId, int amount) {
-    // Check if the amount is zero for deletion, else update
     if (amount <= 0) {
       FirebaseFirestore.instance
           .collection('shoppingLists')
@@ -221,6 +230,154 @@ class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
           .update({'amount': amount});
     }
   }
+<<<<<<< Updated upstream
+=======
+
+  void _fetchShoppingLists() async {
+    final String uid = user!.uid;
+    final QuerySnapshot result = await FirebaseFirestore.instance
+        .collection('shoppingLists')
+        .where('userId', isEqualTo: uid)
+        .orderBy('createdAt', descending: true)
+        .get();
+
+    setState(() {
+      _shoppingLists = result.docs;
+    });
+  }
+
+  void _deleteList(BuildContext context, String docId) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
+
+    try {
+      DocumentSnapshot docSnapshot = await FirebaseFirestore.instance
+          .collection('shoppingLists')
+          .doc(docId)
+          .get();
+      Map<String, dynamic> listData =
+          docSnapshot.data() as Map<String, dynamic>;
+
+      await FirebaseFirestore.instance.collection('deletedShoppingLists').add({
+        ...listData,
+        'deletedAt': FieldValue.serverTimestamp(),
+        'userId': user.uid,
+      });
+
+      final QuerySnapshot deletedListsSnapshot = await FirebaseFirestore
+          .instance
+          .collection('deletedShoppingLists')
+          .where('userId', isEqualTo: user.uid)
+          .orderBy('deletedAt', descending: true)
+          .get();
+
+      if (deletedListsSnapshot.docs.length > 5) {
+        await FirebaseFirestore.instance
+            .collection('deletedShoppingLists')
+            .doc(deletedListsSnapshot.docs.last.id)
+            .delete();
+      }
+
+      await FirebaseFirestore.instance
+          .collection('shoppingLists')
+          .doc(docId)
+          .delete();
+      if (user != null) {
+        _fetchShoppingLists();
+      }
+
+      print("Shopping List Deleted and backup created");
+    } catch (e) {
+      print("Error handling shopping list deletion: $e");
+    }
+  }
+>>>>>>> Stashed changes
+
+  void _fetchSuggestedItems() async {
+    try {
+      // Fetch items from the shopping list
+      var shoppingListSnapshot = await FirebaseFirestore.instance
+          .collection('shoppingLists/${widget.listId}/items')
+          .get();
+
+      // Extract names and prices of items in the shopping list
+      Map<String, double> shoppingListItems = {};
+      for (var doc in shoppingListSnapshot.docs) {
+        var itemData = doc.data() as Map<String, dynamic>;
+        String originalName = itemData['name'];
+        double originalPrice = itemData['price'];
+        shoppingListItems[originalName] = originalPrice;
+      }
+
+      List<Map<String, dynamic>> newItems = [];
+      for (var doc in shoppingListSnapshot.docs) {
+        var itemData = doc.data() as Map<String, dynamic>;
+        String originalName = itemData['name'];
+        double originalPrice = itemData['price'];
+
+        var productsSnapshot = await FirebaseFirestore.instance
+            .collection(
+                'allProducts/${itemData['main_category']}/${itemData['sub_category']}/${itemData['sub_category2']}/Products')
+            .get();
+
+        Map<String, dynamic>? cheapestItem;
+        double maxSimilarity = 0.0;
+
+        for (var productDoc in productsSnapshot.docs) {
+          var productData = productDoc.data();
+          String candidateName = productData['product_name'];
+
+          // Skip if the item is already in the shopping list
+          if (shoppingListItems.containsKey(candidateName)) continue;
+
+          double candidatePrice = productData['product_cheapest_price'];
+
+          // Check if the suggested item is cheaper than the original item
+          if (candidatePrice >= originalPrice) continue;
+
+          // Check if the suggested item price is less than 50% of the original item price
+          if (candidatePrice < 0.5 * originalPrice) continue;
+
+          double similarity = _calculateSimilarity(originalName, candidateName);
+
+          if (similarity > maxSimilarity) {
+            maxSimilarity = similarity;
+            cheapestItem = {
+              'id': productDoc.id, // Store the suggested item ID
+              'product_name': candidateName,
+              'price': candidatePrice,
+              'amount': itemData['amount']
+            };
+          }
+        }
+
+        if (cheapestItem != null) {
+          newItems.add(cheapestItem);
+        }
+      }
+
+      setState(() {
+        suggestedItems = newItems;
+        isLoading = false;
+      });
+    } catch (e) {
+      print("Error fetching suggested items: $e");
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  double _calculateSimilarity(String original, String candidate) {
+    List<String> originalWords = original.toLowerCase().split(' ');
+    List<String> candidateWords = candidate.toLowerCase().split(' ');
+
+    var intersection =
+        originalWords.toSet().intersection(candidateWords.toSet()).length;
+    var union = originalWords.toSet().union(candidateWords.toSet()).length;
+
+    return intersection / union.toDouble();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -231,15 +388,27 @@ class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
           IconButton(
             icon: Icon(Icons.cancel),
             onPressed: () {
+<<<<<<< Updated upstream
               // Cancel action
+=======
+              _deleteList(context, widget.listId);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyShoppingLists(),
+                ),
+              );
+>>>>>>> Stashed changes
             },
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
               itemCount: items.length,
               itemBuilder: (context, index) {
                 var item = items[index];
@@ -251,20 +420,59 @@ class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
                       backgroundColor: Colors.white,
                       child: Icon(Icons.shopping_cart, color: Colors.blue),
                     ),
+<<<<<<< Updated upstream
                     title: Text(item['name']),
                     subtitle: Text(
                         'Amount: ${item['amount']}   Price: \$${item['price'].toStringAsFixed(2)}'),
+=======
+                    title: Text(
+                      item['name'],
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Amount: ${item['amount']}   Price: ${item['price'].toStringAsFixed(2)}₺',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+>>>>>>> Stashed changes
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
                         IconButton(
+<<<<<<< Updated upstream
                           icon: Icon(Icons.add),
+=======
+                          icon: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue[100],
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(Icons.add, color: Colors.black),
+                          ),
+>>>>>>> Stashed changes
                           onPressed: () {
                             _updateItemAmount(item['id'], item['amount'] + 1);
                           },
                         ),
                         IconButton(
+<<<<<<< Updated upstream
                           icon: Icon(Icons.remove),
+=======
+                          icon: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue[100],
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(Icons.remove, color: Colors.red),
+                          ),
+>>>>>>> Stashed changes
                           onPressed: () {
                             _updateItemAmount(item['id'], item['amount'] - 1);
                           },
@@ -275,6 +483,7 @@ class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
                 );
               },
             ),
+<<<<<<< Updated upstream
           ),
           Container(
             padding: EdgeInsets.all(16),
@@ -309,12 +518,159 @@ class _ShoppingListDetailsPageState extends State<ShoppingListDetailsPage> {
                   borderRadius: BorderRadius.circular(30),
                 ),
                 padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+=======
+            Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Suggested Items',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  isLoading
+                      ? Center(child: CircularProgressIndicator())
+                      : suggestedItems.isEmpty
+                          ? Center(child: Text('No suggested items'))
+                          : ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: suggestedItems.length,
+                              itemBuilder: (context, index) {
+                                var item = suggestedItems[index];
+                                return Card(
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: 16, vertical: 4),
+                                  color: Colors.lightBlue[200],
+                                  child: ListTile(
+                                    leading: CircleAvatar(
+                                      backgroundColor: Colors.white,
+                                      child: Icon(Icons.shopping_cart,
+                                          color: Colors
+                                              .blue), // Shopping cart icon
+                                    ),
+                                    title: Text(
+                                      item['product_name'],
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      'Price: ${item['price'].toStringAsFixed(2)}₺',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    trailing: IconButton(
+                                      icon: Container(
+                                        padding: EdgeInsets.all(8),
+                                        decoration: BoxDecoration(
+                                          color: Colors.lightBlue[100],
+                                          shape: BoxShape.circle,
+                                        ),
+                                        child: Icon(Icons.add,
+                                            color: Colors.black),
+                                      ),
+                                      onPressed: () {
+                                        // Create a new item with the details of the suggested item
+                                        Map<String, dynamic> newItem = {
+                                          'name': item['product_name'],
+                                          'amount': item['amount'],
+                                          'price': item['price'],
+                                        };
+
+                                        // Add the new item to the shopping list
+                                        FirebaseFirestore.instance
+                                            .collection('shoppingLists')
+                                            .doc(widget.listId)
+                                            .collection('items')
+                                            .add(newItem)
+                                            .then((value) {
+                                          // Item added successfully
+                                          print('Item added to the list');
+                                        }).catchError((error) {
+                                          // Error adding item to the list
+                                          print(
+                                              'Error adding item to the list: $error');
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                ],
+>>>>>>> Stashed changes
               ),
             ),
-          ),
-        ],
+            Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 25.0), // Adjust the padding as needed
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue[100],
+                        border: Border.all(color: Colors.lightBlue),
+                        borderRadius: BorderRadius.circular(27),
+                      ),
+                      child: Text(
+                        'Total: ${total.toStringAsFixed(2)}₺',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ShowSuggestedListsPage(
+                                      listId: widget.listId,
+                                    )));
+                      },
+                      child: Text(
+                        'Show Suggestions',
+                        style: TextStyle(
+                          color: Colors.white, // Text color
+                          fontSize: 16, // Font size
+                          fontWeight: FontWeight.bold, // Font weight
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue, // Background color of the button
+                        onPrimary: Colors
+                            .white, // Text color when the button is in focus/hover/pressed
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(27),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 35, vertical: 16),
+                        textStyle: TextStyle(
+                          fontSize:
+                              16, // This can be set here or directly in the Text widget
+                          fontWeight: FontWeight
+                              .bold, // This can be set here or directly in the Text widget
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ],
+        ),
       ),
       bottomNavigationBar: navigation_bar_bottom(context),
     );
   }
 }
+//
